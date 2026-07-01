91781bce7ecbf84690434bc994c1e16a21a6ba98
sgccofficial <sujithprasanth21@gmail.com>
2026-07-01 19:35:27 +0530

    Add badge to timer notification

--- src/components/TimerPage.tsx
+++ src/components/TimerPage.tsx
@@ -261,12 +261,14 @@ export function TimerPage() {
         navigator.serviceWorker.ready.then(reg => {
           reg.showNotification('Timer Started...', {
             body: `${title}'s timer is set for ${timeString}.`,
-            icon: `/icon-192.png`
+            icon: '/icon-192.png',
+            badge: '/badge-192.png'
           });
         }).catch(e => {
           new Notification('Timer Started...', {
             body: `${title}'s timer is set for ${timeString}.`,
-            icon: `/icon-192.png`
+            icon: '/icon-192.png',
+            badge: '/badge-192.png'
           });
         });
       } else if ('Notification' in window && Notification.permission !== 'denied') {
